a6db6044c6838ecd8be551de95ea5cc30a9fc9ec
osamaserhani <37387407+osamaserhani@users.noreply.github.com>
2018-03-25 02:20:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
 var books = [
   {
-    title: 'React Quickly: Painless web apps with React, JSX, Redux, and GraphQL',
+    title: '[Title  : React Quickly: Painless web apps with React, JSX, Redux, and GraphQL]',
     url: 'google.com',
-    date_published: 'Sep 28, 2017',
-    author: 'Azat Mardan',
-    reviews: 34,
-    rate: 4.7,
+    date_published: '[date_published  : Sep 28, 2017]     ',
+    author: '[author  :Azat Mardan]     ',
+    reviews: '[reviews  : 34]     ',
+    rate: '[rate  : 4.7]     ',
     img: 'https://images-na.ssl-images-amazon.com/images/I/5159foIB0EL._AC_US218_.jpg'
   }, {
-    title: 'The Principles of Object-Oriented JavaScript',
+    title: '[Title  : React Quickly: Painless web apps with React, JSX, Redux, and GraphQL]',
     url: 'google.com',
-    date_published: 'Feb 14, 2014',
-    author: 'Nicholas C. Zakas',
-    reviews: 56,
-    rate: 4.6,
-    img: 'https://images-na.ssl-images-amazon.com/images/I/51nCY21njKL._AC_US218_.jpg'
+    date_published: '[date_published  : Sep 28, 2017]     ',
+    author: '[author  :Azat Mardan]     ',
+    reviews: '[reviews  : 34]     ',
+    rate: '[rate  : 4.7]     ',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/5159foIB0EL._AC_US218_.jpg'
   },
+  {    title: '[Title  : React Quickly: Painless web apps with React, JSX, Redux, and GraphQL]',
+   url: 'google.com',
+   date_published: '[date_published  : Sep 28, 2017]     ',
+   author: '[author  :Azat Mardan]     ',
+   reviews: '[reviews  : 34]     ',
+   rate: '[rate  : 4.7]     ',
+   img: 'https://images-na.ssl-images-amazon.com/images/I/5159foIB0EL._AC_US218_.jpg'
+  }
 
   // add more book items here
 ]
@@ -37,16 +45,34 @@ function createBookItem(bookObj) {
       </li>
     */
   var liElem = document.createElement('li')
-  var a = document.createElement('a')
+  var ulElem = document.createElement('ul')
+  var liElem2 = document.createElement('li')
   var img = document.createElement('img')
+
+  var p = document.createElement('p')
+  var p1 = document.createElement('p')
   var aText = document.createTextNode(bookObj.title)
-  a.appendChild(aText)
-  a.href = bookObj.url
-  a.title = bookObj.title
+  var aText1 = document.createTextNode(bookObj.date_published)
+  var aText2 = document.createTextNode(bookObj.author)
+  var aText3 = document.createTextNode(bookObj.reviews)
+  var aText4 = document.createTextNode(bookObj.rate)
+
   img.src=bookObj.img
-  
-  liElem.appendChild(a)
+
+  p1.appendChild(aText)
+  p1.title = bookObj.title
+  p.appendChild(aText2)
   liElem.appendChild(img)
+
+  liElem.appendChild(p1)
+liElem2.appendChild(aText1)
+  liElem2.appendChild(aText2)
+  liElem2.appendChild(aText3)
+  liElem2.appendChild(aText4)
+  liElem2.appendChild(aText2)
+
+  ulElem.appendChild(liElem2)
+  liElem.appendChild(ulElem)
 
 
   // create the remaining elements
@@ -63,8 +89,10 @@ function upDateDOM() {
   var ulBooks = document.getElementById('books-list')
   ulBooks.innerHTML = ''
   for (item of books) {
+    
     ulBooks.appendChild(createBookItem(item))
   }
+ 
 }
 
 // Sort by, select event
